e55a8dcfff8571577afb2b12107de02a4f83b24c
Martin Jakt <lmjakt@gmail.com>
2024-10-30 12:33:28 +0100

    Wrapper functions that count kmer to files
    Added two functions:
    
    1. count.kmers: counts kmers and writes counts to a binary encoded file
    2. read.kmers: reads counts in files written by count.kmers
    
    Note that the count.kmers function will be renamed in the next commit.

--- kmer_spans.R
+++ kmer_spans.R
@@ -1,5 +1,9 @@
 dyn.load( paste(dirname(sys.frame(1)$ofile), "src/kmer_spans.so", sep="/") )
 
+## this defines a magic number that identifies files containing
+## kmer counts.
+kmer.magic <- function(){ 720531L }
+
 ## this counts kmers for a single k;
 ## seq should be a character vector;
 ## the kmers will be counted for the combined
@@ -52,4 +56,63 @@ kmer.low.comp.regions <- function(seq, k, min.w, min.score, thr=0.75){
 ## resulting from the binary encoding of nucleotide identities used.
 kmer.seq <- function(k){
     .Call("kmer_seq_r", as.integer(k))
+}
+
+## this will simply read in a load of sequence files and then output counts to a file
+## specified by the prefix.
+## The files will be binary encoded as 32 bit integers.
+## each file can hold data from several k-mers; The file will contain (offsets given in bytes)
+## 0: signed integer: magic number identifying the file as correct
+## 4: signed integer: number of k's (n) counted
+## 8: signed integer: number of k-mers for each k
+## 8 + n * 4: signed integers: the counts for the first k-mer.
+##
+## the actual values of k can be derived as: log2(length) / 2.
+## returns a list of in and output files and information about the sequence
+count.kmers <- function(seq.f, out.prefix, k, min.l=1e5, magic=kmer.magic()){
+    out.f <- paste0(out.prefix, "counts_", paste(k, collapse="_"), ".bin")
+    seq.size <- 0
+    seq.fsize <- 0
+    seq.fl <- 0
+    read.count <- function(){
+        seq <- readDNAStringSet(seq.f)
+        seq.size <<- sum(nchar(seq))
+        seq <- seq[ nchar(seq) >= min.l ]
+        seq.fsize <<- sum(nchar(seq))
+        seq.fl <<- length(seq)
+        if(length(seq) < 1)
+            stop("No sequence after length filtering")
+        seq <- as.character(seq)
+        counts <- lapply(k, function(x){
+            kmer.counts(seq, x)
+        })
+        counts
+    }
+    counts <- try(read.count(), silent=TRUE )
+    if(class(counts) == "try-error")
+        return(list(seq.f, NA, seq.size, seq.fsize, seq.fl))
+    con <- file(out.f, open="wb")
+    writeBin(as.integer(magic), con)
+    writeBin(as.integer(length(counts)), con )
+    for(x in counts)
+        writeBin(as.integer(length(x$counts)), con)
+    for(x in counts)
+        writeBin(as.integer(x$counts), con)
+    close(con)
+    list(seq.f, out.f, seq.size, seq.fsize, seq.fl)
+}
+
+read.kmers <- function(fname, magic=kmer.magic()){
+    con <- file(fname, open="rb")
+    m1 <- readBin( con, "integer", n=1 )
+    if(m1 != magic)
+        return(FALSE)
+    kn <- readBin(con, "integer", n=1)
+    if(kn < 1)
+        return(FALSE)
+    ks <- readBin(con, "integer", n=kn)
+    counts <- lapply(ks, function(n){
+        readBin(con, "integer", n=n)
+    })
+    list(k=as.integer(log2(ks) / 2), counts=counts)
 }
